c2d6551ca12d7bb8b15bab6a15e20325354f1bbd
Finn Brady <87070580+Finn-T-Brady@users.noreply.github.com>
2021-08-14 14:12:20 +0100

    asdasdasdasdasdasdasdasd

---
+++ MelExtPyTest.py
@@ -0,0 +1,23 @@
+class MrkvChain:
+    NodesContent = []
+    NodesLookUp =[]
+    NodeWeights = [[]]
+    
+    
+    def SetNodeWeight(self,n,m,i):
+        self.NodeWeight[n][m]=i
+        pass
+    def SetNodeWeights(self,n,i):
+        self.NodeWeight[n]=i
+        pass
+    
+    def ArrayDecomp(self,Arr):
+        pass
+    
+    
+    def _init_(self):
+        pass
+    
+    def _init_(self,Arr):
+        self.ArrayDecomp(Arr)
+        pass
