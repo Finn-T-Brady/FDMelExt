5b93fd777ccdf08cfb738c295088f98d59e6fa06
Finn Brady <87070580+Finn-T-Brady@users.noreply.github.com>
2022-01-12 09:30:53 +0000

    Update MrkvChainTest.py

--- MrkvChainTest.py
+++ MrkvChainTest.py
@@ -1,18 +1,6 @@
-from MrkvChainClass import MrkvChain
-def Markov(self,i=None,append=1,Seed=None, Weights=None):
-    if(self.data==[]):
-        print("Warning: .Markov recieved an empty pattern")
-        return self.new([])
-    Mrkv=MrkvChain(self.data,Seed,Weights)
-    new=[]
-    if append==1:
-        new=self.data
-    if i==None:
-        i=len(self.data)
-    for x in range(0,i):
-        new+=[Mrkv.Next()]
-    del Mrkv
-    return self.new(new)
+from FoxDot import *
+from Functions import Markov
+
 #mock Pattern class
 class TestStr:
     data = []
@@ -26,14 +14,63 @@ class TestStr:
     def __str__(self):
         return self.data.__str__()
 
+    def __iter__(self):
+        return self.data
+
 TestStr.Markov=Markov
 
 #p=TestStr([0,3,6,9,8,7,6,5,4,3,2,1])
 #p=TestStr([])
-p=TestStr([0,3,6,9,8,7,4,1])
 
-print(p)
+print("Test1 Starting")
+A = [0,3,6,9,8,7,4,1]
+p = TestStr(A)
+p.Markov(GiveSeed=0)
+print("Test1 Passed\n")
 
-p.Markov()
+print("Test2 Starting")
+A = [1,3,5,8,11,3,4,7]
 
-print(p)
+p=TestStr(A)
+p.Markov(GiveSeed=0)
+
+assert(list(set(A)) == list(set(p.data)))
+print("Test2 Passed\n")
+
+
+print("Test3 Starting")
+A = [2]
+
+p = TestStr(A)
+p.Markov(GiveSeed=0)
+
+assert(A==p.data)
+print("Test3 Passed\n")
+
+print("Test4 Starting")
+A = [1,1,1,1,1,1]
+
+p = TestStr(A)
+p.Markov(GiveSeed=0)
+
+assert(A==p.data)
+print("Test4 Passed\n")
+
+print("Test5 Starting")
+A = [2.4,3.1,6.7]
+
+p = TestStr(A)
+p.Markov(GiveSeed=0)
+assert(list(set(A)) == list(set(p.data)))
+print("Test5 Passed\n")
+
+print("Test6 Starting")
+A=[1,3,4,5,7,3,8]
+
+p=TestStr(A)
+q=TestStr(A)
+
+p.Markov(Seed=94714)
+q.Markov(Seed=94714)
+assert(p.data==q.data)
+print("Test6 Passed\n")
